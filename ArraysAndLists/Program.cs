0be3bdbda840d5796c7892cdc8bfb57810a99c74
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-06-04 14:55:20 -0400

    Exercie 2 complete

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            //* Create two Lists of type int.
+            var evens = new List<int>();
+            var odds = new List<int>();
+
+            /*Name one List "evens"
+            * Name the other List "odds"
+            */
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +26,33 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
 
             /* Now using foreach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+                * display each List of even and odd numbers
+                *
+                * Try to be creative in your display
+                */
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"Yup, {item} is an Even number!");
+            }
+            foreach (var item in odds)
+            {
+                Console.WriteLine($"True, {item} is an Odd number!");
+            }
+
+            
         }
     }
 }
